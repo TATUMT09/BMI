ee17657b1970975dd9a962a7760b2201c995fc3f
unknown <sunnatovasadbek004@gamil.com>
2026-03-10 13:43:15 +0500

    update project

--- src/main/kotlin/com/example/BMI/OpenConfig.kt
+++ src/main/kotlin/com/example/BMI/OpenConfig.kt
@@ -3,6 +3,9 @@ package com.example.BMI
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
 import org.springframework.web.reactive.function.client.WebClient
 
 @Configuration
@@ -14,4 +17,20 @@ class OpenAiConfig {
             .baseUrl("https://api.openai.com/v1")
             .build()
     }
+}
+
+@Bean
+fun corsConfigurationSource(): CorsConfigurationSource {
+
+    val configuration = CorsConfiguration()
+
+    configuration.allowedOriginPatterns = listOf("*")
+    configuration.allowedMethods = listOf("GET","POST","PUT","DELETE","OPTIONS")
+    configuration.allowedHeaders = listOf("*")
+    configuration.allowCredentials = true
+
+    val source = UrlBasedCorsConfigurationSource()
+    source.registerCorsConfiguration("/**", configuration)
+
+    return source
 }
